059671fc5c1a17652bec40a33158ac463b929d51
lai2-24 <lai2-24@itismeucci.com>
2025-10-15 09:33:09 +0200

    server finito

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,8 @@
 package com.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.Buffer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
